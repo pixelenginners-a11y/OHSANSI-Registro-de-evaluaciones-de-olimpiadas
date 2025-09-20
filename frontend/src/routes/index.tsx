22eb9879e5d4eaa347a277f1c1e67f5308d6638b
adrisad <22082002andrademedina@gmail.com>
2025-09-20 19:06:09 -0400

    feat(frontend): instalacion de tailwind

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,3 +1,4 @@
+import '../App.css'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
@@ -6,8 +7,16 @@ export const Route = createFileRoute('/')({
 
 function Index() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-white shadow-lg rounded-2xl p-6 text-center">
+        <h3 className="text-2xl font-bold text-gray-800 mb-2">
+          Welcome Home!
+        </h3>
+        <p className="text-gray-600">Has iniciado sesión correctamente 🚀</p>
+        <button className="mt-4 px-4 py-2 bg-blue-600 text-white font-medium rounded-lg hover:bg-blue-700 transition">
+          Explorar
+        </button>
+      </div>
     </div>
   )
 }
