976880b1bdae54c1a3fae86b58183b4286954c41
Jhoestar <202202883@est.umss.edu>
2025-09-20 20:33:29 -0400

    feat(backend): creando las tablas en las migraciones

--- backend/database/migrations/2025_09_20_235715_create_areas_table.php
+++ backend/database/migrations/2025_09_20_235715_create_areas_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('areas', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100)->unique();
+            $table->string('description')->nullable();
+            $table->boolean('active')->default(true);
             $table->timestamps();
         });
     }
