4985ad2dba736eae5a3a4c92667475f992692a5b
adrisad <22082002andrademedina@gmail.com>
2025-09-21 16:45:20 -0400

    style(frontend): creacion del componente header y estilo al home

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,17 +1,11 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import Footer from '../components/Footer'
+import Header from '../components/Header'
 
 const RootLayout = () => (
   <>
-    <div className="p-2 flex gap-2">
-      <Link to="/" className="[&.active]:font-bold">
-        Home
-      </Link>{' '}
-      <Link to="/about" className="[&.active]:font-bold">
-        About
-      </Link>
-    </div>
+    <Header />
     <hr />
     <Outlet />
     <Footer />
