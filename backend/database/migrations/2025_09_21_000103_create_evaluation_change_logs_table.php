751a26d3fe4b24b3de1ddfb0a4194990421e0ee1
Jhoestar <202202883@est.umss.edu>
2025-09-22 17:15:48 -0400

    feat(backend): reestructurar las migraciones

--- backend/database/migrations/2025_09_21_000103_create_evaluation_change_logs_table.php
+++ backend/database/migrations/2025_09_21_000103_create_evaluation_change_logs_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('evaluation_change_logs', function (Blueprint $table) {
             $table->id();
-            $table->foreingId('evaluation_id')->references('id')->on('evaluations')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('evaluation_id')->references('id')->on('evaluations')->onUpdate('cascade')->onDelete('cascade');
             $table->foreignId('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
             $table->decimal('previous_score', 5, 2);
             $table->decimal('new_score', 5, 2);
