6e6dc757dca2ff2d33bcea6b1af032d37a1a79b5
Jhoestar <202202883@est.umss.edu>
2025-09-22 17:14:42 -0400

    feat(backend): implementar relaciones en los modelos

--- backend/app/Models/Area.php
+++ backend/app/Models/Area.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Area extends Model
 {
-    //
+    public function medalParameters()
+    {
+        return $this->hasOne(MedalParameter::class);
+    }
+
+    public function evaluatorAreas()
+    {
+        return $this->hasMany(EvaluatorArea::class);
+    }
+
+    public function inscriptions()
+    {
+        return $this->belongsTo(Inscription::class);
+    } 
+
+    public function grades()
+    {
+        return $this->hasMany(Grade::class);
+    }
+
+    public function responsable()
+    {
+        return $this->belongsTo(User::class, 'responsable_id');
+    }
 }
--- backend/app/Models/EvaluatorArea.php
+++ backend/app/Models/EvaluatorArea.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class EvaluatorArea extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function area()
+    {
+        return $this->belongsTo(Area::class);
+    }
 }
