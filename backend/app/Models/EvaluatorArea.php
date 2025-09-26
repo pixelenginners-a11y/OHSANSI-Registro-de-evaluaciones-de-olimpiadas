c1859c72cefe1f3dc755e98fb6229034c7d5cff7
Jhoestar <202202883@est.umss.edu>
2025-09-26 13:39:51 -0400

    fix(backend): permitir fillable en evaluatorArea

--- backend/app/Models/EvaluatorArea.php
+++ backend/app/Models/EvaluatorArea.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class EvaluatorArea extends Model
 {
+    protected $fillable = ['user_id', 'area_id'];
+    
     public function user()
     {
         return $this->belongsTo(User::class);
