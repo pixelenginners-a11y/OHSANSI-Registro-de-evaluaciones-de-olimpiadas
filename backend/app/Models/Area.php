0e3a1c553bcf1d1613a58d6c16f309bf3f9f7903
Jhoestar <202202883@est.umss.edu>
2025-09-26 19:22:14 -0400

    feat(backend): agregar propiedad softDelete

--- backend/app/Models/Area.php
+++ backend/app/Models/Area.php
@@ -3,9 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Area extends Model
 {
+    use SoftDeletes;
+
+    protected $fillable = [
+      'name', 
+      'description', 
+      'active'
+    ];
+
     public function medalParameters()
     {
         return $this->hasOne(MedalParameter::class);
--- backend/database/migrations/2025_09_20_235749_create_grades_table.php
+++ backend/database/migrations/2025_09_20_235749_create_grades_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->boolean('active')->default(true);
             $table->timestamps();
             $table->unique(['name']);
+            $table->softDeletes();
         });
     }
 
