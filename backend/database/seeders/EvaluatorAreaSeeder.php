3f68b9c3642cb7c3eae15e8686481ed1cfffbce0
Jhoestar <202202883@est.umss.edu>
2025-09-26 11:25:50 -0400

    feat(backend): rellenar evaluator_areas segun los datos iniciales creados

--- backend/database/seeders/EvaluatorAreaSeeder.php
+++ backend/database/seeders/EvaluatorAreaSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\EvaluatorArea;
+use App\Models\User;
 
 class EvaluatorAreaSeeder extends Seeder
 {
@@ -12,6 +14,14 @@ class EvaluatorAreaSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $evaluators = User::whereHas('role', fn($q) => $q->where('name', 'Evaluador'))->get();
+
+        foreach ($evaluators as $index => $evaluator) {
+            $areaId = ($index % 5) + 1;
+            EvaluatorArea::create([
+                'user_id' => $evaluator->id,
+                'area_id' => $areaId,
+            ]);
+        }
     }
 }
