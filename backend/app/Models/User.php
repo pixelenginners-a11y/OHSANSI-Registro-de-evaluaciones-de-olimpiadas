9a190f28bd8188a091e1d6fcdea653340716b8c1
pixelenginners-a11y <pixelenginners@gmail.com>
2025-09-28 11:42:03 -0400

    Revert "fix(backend): correccion de algunos archivos"
    This reverts commit efe52d25b11dccdd86a9d812040da49074fe1719.

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -9,23 +9,35 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-
+    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
     protected $fillable = [
-        'full_name',
-        'username',
+        'name',
         'email',
         'password',
-        'phone',
-        'role_id'
     ];
 
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
     protected function casts(): array
     {
         return [
