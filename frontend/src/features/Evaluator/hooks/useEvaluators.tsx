5aedca0bd4e3accc5e949819e8a7d9ed8520b0d0
pixelenginners-a11y <pixelenginners@gmail.com>
2025-09-28 11:42:03 -0400

    Revert "fix(frontend): agregar barrels a evaluator"
    This reverts commit df775fa75fb51068f143515251293d21e12a7153.

---
+++ frontend/src/features/Evaluator/hooks/useEvaluators.tsx
@@ -0,0 +1,37 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import api from "../../../api/axios";
+import type { EvaluatorResponse, EvaluatorUpdate } from "../types/evaluator";
+
+export const useGetEvaluators = () => {
+
+  return useQuery({
+    queryKey: ["evaluators"],
+    queryFn: async () => {
+      const res = await api.get("/evaluators");
+      if (!res) throw new Error("Error get evaluators");
+      return res.data;
+    }
+  })
+}
+
+export const useGetEvaluatorById = (id: number) => {
+
+  return useQuery<EvaluatorResponse>({
+    queryKey: ["evaluator", id],
+    queryFn: async () => {
+      const res = await api.get(`/evaluators/${id}`);
+      if (!res) throw new Error("Error get evaluator by id");
+      return res.data;
+    }
+  })
+}
+
+export const useUpdateEvaluator = (id: number) => {
+  return useMutation<EvaluatorUpdate>({
+    mutationFn: async () => {
+      const res = await api.get(`/evaluators/${id}`);
+      if (!res) throw new Error("Error get evaluator by id");
+      return res.data;
+    }
+  })
+}
