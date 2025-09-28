df025331296e97ef3b143b5522612aff20344815
pixelenginners-a11y <pixelenginners@gmail.com>
2025-09-28 11:42:03 -0400

    Revert "fix(backend): agregar todos los roles al seeder"
    This reverts commit fa0bc344f3b58c5543d631b24fc143c00fe10b9a.

--- backend/database/seeders/RoleSeeder.php
+++ backend/database/seeders/RoleSeeder.php
@@ -10,17 +10,6 @@ class RoleSeeder extends Seeder
 {
     public function run(): void
     {
-        $roles = [
-          ['name' => 'Administrador', 'description' => 'Usuario con todos los permisos'],
-          ['name' => 'Evaluador', 'description' => 'Usuario que puede evaluar inscripciones'],
-          ['name' => 'Responsable Academico', 'description' => 'Usuario que gestiona un área específica'],
-        ];
-        
-        foreach ($roles as $roleData) {
-            Role::updateOrCreate(
-              ['name' => $roleData['name']], 
-              ['description' => $roleData['description']]
-            );
-        }
+        Role::created(['name' => 'Evaluador']);
     }
 }
