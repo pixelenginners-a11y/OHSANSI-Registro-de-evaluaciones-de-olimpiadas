a916838ca4a0db9cdad5e15246d767836bfa9d98
pixelenginners-a11y <pixelenginners@gmail.com>
2025-09-28 11:42:03 -0400

    Revert "feat(backend): Creacion de seeder de rol de evaluador"
    This reverts commit 3887aead0956c2d35cfe5ae364af3f17fb98c248.

--- backend/database/seeders/RoleSeeder.php
+++ backend/database/seeders/RoleSeeder.php
@@ -4,12 +4,14 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Role;
 
 class RoleSeeder extends Seeder
 {
+    /**
+     * Run the database seeds.
+     */
     public function run(): void
     {
-        Role::created(['name' => 'Evaluador']);
+        //
     }
 }
