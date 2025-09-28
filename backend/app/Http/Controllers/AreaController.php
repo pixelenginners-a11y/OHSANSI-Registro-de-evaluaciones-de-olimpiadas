ff9af817f03e3bdee44696ba86c82e0dec678b6d
Jhoestar <202202883@est.umss.edu>
2025-09-27 22:47:43 -0400

    feat(backend): agregar las rutas y el controlador para las areas

--- backend/app/Http/Controllers/AreaController.php
+++ backend/app/Http/Controllers/AreaController.php
@@ -2,64 +2,84 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Http\Requests\StoreAreaRequest;
+use App\Http\Requests\UpdateAreaRequest;
 use App\Models\Area;
 use Illuminate\Http\Request;
+use App\Services\AreaService;
 
 class AreaController extends Controller
 {
+    public function __construct(
+        protected AreaService $areaService
+    ) {}
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        $areas = $this->areaService->getAllWithGradesAndMedals();
+        return response()->json($areas);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreAreaRequest $request)
     {
-        //
+        $area = $this->areaService->create($request->validated());
+        return response()->json([
+            'message' => 'Area creada correctamente',
+            'data' => $area,
+        ], 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Area $area)
+    public function show(int $id)
     {
-        //
-    }
+        $area = $this->areaService->getById($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Area $area)
-    {
-        //
+        if (!$area) {
+            return response()->json(['message' => 'Area no encontrada'], 404);
+        }
+
+        return response()->json($area);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Area $area)
+    public function update(UpdateAreaRequest  $request, int $id)
     {
-        //
+        $data = $request->validated();
+        $area = $this->areaService->update($id, $data);
+
+        if (!$area) {
+            return response()->json(['message' => 'Area no encontrada'], 404);
+        }
+
+        return response()->json([
+            'message' => 'Area actualizada correctamente',
+            'data' => $area,
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Area $area)
+    public function destroy(int $id)
     {
-        //
+        $deleted = $this->areaService->delete($id);
+
+        if (!$deleted) {
+            return response()->json(['message' => 'Area no encontrada'], 404);
+        }
+
+        return response()->json([
+            'message' => 'Area eliminada correctamente',
+        ]);
     }
 }
