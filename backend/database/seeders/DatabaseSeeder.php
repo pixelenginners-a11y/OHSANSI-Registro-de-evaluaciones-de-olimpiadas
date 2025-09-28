ab02afb7481730a1dd984c79741345d68e745cdd
pixelenginners-a11y <pixelenginners@gmail.com>
2025-09-28 11:42:03 -0400

    Revert "feat(backend): creacion de usuarios iniciales"
    This reverts commit 978a0fee3249c098e5235f5e7fbd47dd764998c3.

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,10 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
         $this->call(RoleSeeder::class);
-        $this->call(AreaSeeder::class);
-        $this->call(UserSeeder::class);
-        $this->call(EvaluatorAreaSeeder::class);
-        $this->call(GradeSeeder::class);
     }
 }
