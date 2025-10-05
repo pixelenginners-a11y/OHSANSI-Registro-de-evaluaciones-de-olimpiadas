6cd73bc1386cb9edd00d726896b4e4de19ada5c5
adrisad <22082002andrademedina@gmail.com>
2025-10-04 20:53:58 -0400

    feat(frontend): actualizar servicios de autenticación API

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,5 +1,15 @@
 import api from "./axios";
+import type { LoginResponse, User } from "../features/login/types";
 
 export const login = (email: string, password: string) => {
-  return api.post("/login", { email, password });
+  return api.post<LoginResponse>("/login", { email, password });
+};
+
+export const getMe = () => {
+  const token = localStorage.getItem("token");
+  return api.get<User>("/me", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 };
