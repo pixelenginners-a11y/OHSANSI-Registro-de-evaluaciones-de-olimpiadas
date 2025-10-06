dba2d93028fa1af2d810e3f3bd3facfab07e1056
Jhoestar <202202883@est.umss.edu>
2025-10-06 00:04:08 -0400

    feat(frontend): agregar hooks para administrar usuarios

---
+++ frontend/src/features/AdministratorUsers/hooks/useResponsibleQueries.tsx
@@ -0,0 +1,42 @@
+import { useQuery } from "@tanstack/react-query";
+
+import api from "../../../api/axios";
+
+export const useGetAcademics = (page: number = 1) => {
+
+  return useQuery({
+    queryKey: ["getAcademics", page],
+    queryFn: async () => {
+      const res = await api.get(`/academics?page=${page}`);
+      return res.data;
+    }
+  })
+}
+
+export const useGetAcademicsById = (id: number) => {
+
+  return useQuery({
+    queryKey: ["academics", id],
+    queryFn: async () => {
+      const res = await api.get(`/academics/${id}`);
+      return res.data;
+    }
+  })
+}
+
+export const useSearchAcademics = (query: string, areaId: string) => {
+
+  return useQuery({
+    queryKey: ["searchAcademics", query, areaId],
+    queryFn: async () => {
+      const params: Record<string, string> = {};
+      if (query) params.query = query;
+      if (areaId) params.areaId = areaId
+      const res = await api.get("academics/search", {
+        params,
+      });
+      return res.data;
+    },
+    enabled: !!query || !!areaId,
+  })
+}
