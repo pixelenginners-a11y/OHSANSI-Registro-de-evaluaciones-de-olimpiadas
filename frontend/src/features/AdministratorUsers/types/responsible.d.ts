36aa39a3ac764abff97fe34030ee4476269579b0
Jhoestar <202202883@est.umss.edu>
2025-10-06 00:03:30 -0400

    feat(frontend): agregar tipos para administrar usuarios

---
+++ frontend/src/features/AdministratorUsers/types/responsible.d.ts
@@ -0,0 +1,27 @@
+import { PaginatedResponse } from "../../../types/Pagination";
+
+export interface Responsable {
+  id: number;
+  full_name: string;
+  username: string;
+  email: string;
+  area: string | null;
+  area_id?: number | string | null;
+  active: boolean;
+  phone?: string;
+}
+
+export interface ResponsableEdit extends Omit<Responsable, 'id'> { }
+
+export interface ResponsableParcialEdit extends Partial<Omit<Responsable, 'id'>> { }
+
+export interface ResponsableCreate extends Omit<Responsable, 'id' | 'area'> {
+  password: string;
+}
+
+export interface Column {
+  key: keyof Responsable;
+  label: string;
+}
+
+export interface PaginatedResponsablesResponse extends PaginatedResponse<Responsable> { }
