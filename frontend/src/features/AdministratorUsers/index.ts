315b2678a70c5b123ad1c393a5dc100ded75e735
Jhoestar <202202883@est.umss.edu>
2025-10-06 00:05:45 -0400

    chore(frontend): agregar index a features/administratorUsers

--- frontend/src/features/AdministratorUsers/index.ts
+++ frontend/src/features/AdministratorUsers/index.ts
@@ -1,3 +1,5 @@
 export { default as EvaluatorPage } from './pages/EvaluatorPage';
 export { default as InscriptionPage } from './pages/InscriptionPage';
 export { default as ResponsablePage } from './pages/ResponsablePage';
+export * from './hooks/index';
+export * from './types/index';
