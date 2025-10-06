50e8e67fb1f96cfca8ceb2f3db1c35f640cb6211
Jhoestar <202202883@est.umss.edu>
2025-10-06 00:06:51 -0400

    feat(frontend: agregar componente para usar iconos de lucide-icons)

--- frontend/src/components/Icon.tsx
+++ frontend/src/components/Icon.tsx
@@ -3,11 +3,12 @@ import { icons, type IconName } from '../lib/Icons';
 type IconProps = {
   name: IconName;
   className?: string;
+  size?: number;
 }
 
-const Icon = ({ name, className = "w-5 h-5" }: IconProps) => {
+const Icon = ({ name, className, size = 16 }: IconProps) => {
   const IconComponent = icons[name];
-  return IconComponent ? <IconComponent className={className} /> : null;
+  return IconComponent ? <IconComponent className={className} size={size} /> : null;
 };
 
 export default Icon;
