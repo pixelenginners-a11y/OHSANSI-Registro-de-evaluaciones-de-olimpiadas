b897f87c26af7f58618c8c1482f787b7b8aa6ad3
adrisad <22082002andrademedina@gmail.com>
2025-10-09 14:56:47 -0400

    refactor(frontend): refactorizando interfaz

--- frontend/src/features/login/pages/loginPage.tsx
+++ frontend/src/features/login/pages/loginPage.tsx
@@ -1,55 +1,14 @@
-// src/features/login/pages/loginPage.tsx
-import { useState, type FormEvent } from "react";
-import { useNavigate } from "@tanstack/react-router";
-import { Header } from "../components/Header";
-import { PageTitle } from "../components/PageTitle";
-import { LoginPanel } from "../components/LoginPanel";
-import { useLogin } from "../hooks/useLogin";
-import { getMe } from "../../../api/auth";
+import { Form } from "../components/Form";
+import { LoginLayout } from "../components/LoginLayout";
+import { Title } from "../components/Title";
 
 export default function LoginPage() {
-  const navigate = useNavigate();
-  const { loginAsync, loading, error } = useLogin();
-
-  const [email, setEmail] = useState("");
-  const [pwd, setPwd] = useState("");
-
-  const canSubmit = email.trim().length > 3 && pwd.trim().length > 0;
-
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (!canSubmit || loading) return;
-
-    try {
-      await loginAsync({ email, password: pwd });
-      const response = await getMe();
-      const userRole = response?.data.role_id;
-
-      if (userRole === 1) {
-        navigate({ to: "/admin", replace: true });
-      } else {
-        // Otros roles van a página en construcción
-        navigate({ to: "/shared/construccion", replace: true });
-      }
-    } catch {
-      // Error manejado por el hook
-    }
-  };
-
   return (
     <div className="min-h-screen bg-[var(--color-primary-dark)] text-[var(--color-primary-light)]">
-      <Header />
-      <PageTitle />
-      <LoginPanel
-        email={email}
-        password={pwd}
-        loading={loading}
-        canSubmit={canSubmit}
-        error={error}
-        onEmailChange={setEmail}
-        onPasswordChange={setPwd}
-        onSubmit={handleSubmit}
-      />
+      <Title />
+      <LoginLayout>
+        <Form/>
+      </LoginLayout>
     </div>
   );
 }
