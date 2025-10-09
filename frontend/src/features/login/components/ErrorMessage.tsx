ebc801d2e873982181c730d0de5e6eb4e345abd1
adrisad <22082002andrademedina@gmail.com>
2025-10-09 14:55:53 -0400

    refactor(frontend): eliminando archivos no usados

--- frontend/src/features/login/components/ErrorMessage.tsx
+++
@@ -1,9 +0,0 @@
-type ErrorMessageProps = {
-  message: string;
-};
-
-export const ErrorMessage = ({ message }: ErrorMessageProps) => (
-  <div className="mb-4 rounded-lg border border-red-300 bg-red-50 text-red-700 px-3 py-2 text-sm">
-    {message}
-  </div>
-);
