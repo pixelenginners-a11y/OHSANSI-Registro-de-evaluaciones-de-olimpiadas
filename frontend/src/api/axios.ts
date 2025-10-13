a628b43267d73fbabf1dca92633ab15282835da5
adrisad <22082002andrademedina@gmail.com>
2025-10-12 21:40:18 -0400

    agregando conexion al deploy de backend

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8200/api',
+  baseURL: 'https://ohsansi-registro-de-evaluaciones-de.onrender.com/api',
   headers: { Accept: "application/json" },
+  timeout: 10000, // 10 segundos
 });
 
 export default api;
