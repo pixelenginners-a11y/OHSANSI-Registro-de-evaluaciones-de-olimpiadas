04ca37eee70a0112b5d4779ede05e5d01ee88ebc
adrisad <22082002andrademedina@gmail.com>
2025-10-19 20:27:01 -0400

    feat(backend): correccion de propiedades que no estan contenpladas en el modelo

--- backend/app/Services/OlympianService.php
+++ backend/app/Services/OlympianService.php
@@ -17,7 +17,6 @@ class OlympianService
         $validated = $req->validate([
             'full_name' => 'required|string|max:100',
             'identity_document' => 'required|string|max:20|unique:olympians,identity_document',
-            'legal_guardian_contact' => 'required|string|max:100',
             'educational_institution' => 'required|string|max:100',
             'department' => 'required|string|max:50',
             'academic_tutor' => 'nullable|string|max:100',
@@ -30,10 +29,8 @@ class OlympianService
         $validator = Validator::make($data, [
             'rows.*.full_name' => 'required|string|max:100',
             'rows.*.identity_document' => 'required|string|max:20|unique:olympians,identity_document',
-            'rows.*.legal_guardian_contact' => 'required|string|max:100',
             'rows.*.educational_institution' => 'required|string|max:100',
             'rows.*.department' => 'required|string|max:50',
-            'rows.*.school_grade' => 'required|string|max:50',
             'rows.*.academic_tutor' => 'nullable|string|max:100',
         ]);
         $validated = $validator->validate();
