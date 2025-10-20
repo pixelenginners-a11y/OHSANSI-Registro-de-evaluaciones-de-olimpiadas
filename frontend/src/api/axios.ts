504910164b7f2ce1e4964626d3e4ec2fa7ea9744
adrisad <22082002andrademedina@gmail.com>
2025-10-20 10:16:39 -0400

    fix(frontend): tiempo de importacion muy corto

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://ohsansi-registro-de-evaluaciones-de.onrender.com/api',
+  baseURL: 'http://localhost:8200/api',
   headers: { Accept: "application/json" },
-  timeout: 10000, // 10 segundos
+  timeout: 20000, // 10 segundos
 });
 
 export default api;
