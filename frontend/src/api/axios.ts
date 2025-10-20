0fa1c994df2d34ec5369abd1c4735fae746ad470
adrisad <22082002andrademedina@gmail.com>
2025-10-20 10:24:57 -0400

    fix(frontend): correccion ruta de peticiones

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8200/api',
+  baseURL: 'https://ohsansi-registro-de-evaluaciones-de.onrender.com/api',
   headers: { Accept: "application/json" },
   timeout: 20000, // 10 segundos
 });
