d285eef7894f71a6975f422776739a3af0cb7897
adrisad <22082002andrademedina@gmail.com>
2025-11-02 15:12:08 -0400

    feat(frontend): quitando el tiempo de espera para las peticiones

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -3,7 +3,6 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'https://ohsansi-registro-de-evaluaciones-de.onrender.com/api',
   headers: { Accept: "application/json" },
-  timeout: 20000, // 10 segundos
 });
 
 export default api;
