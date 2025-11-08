4c4346b7207224c9d945ea84800fb17a5db5f076
adrisad <22082002andrademedina@gmail.com>
2025-11-08 13:04:40 -0400

    feat(frontend): Control de expiracion de token de jwt

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,9 +1,28 @@
 import axios from 'axios';
+import { jwtDecode } from 'jwt-decode';
 
 const api = axios.create({
   baseURL: 'https://ohsansi-registro-de-evaluaciones-de.onrender.com/api',
   headers: { Accept: "application/json" },
-  timeout: 20000, // 10 segundos
 });
 
-export default api;
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const decoded = jwtDecode<{ exp?: number }>(token);
+      const currentTime = Date.now() / 1000; // de milisegundos a segundos
+
+      // Verificar expiración
+      if (decoded.exp && decoded.exp < currentTime) {
+        localStorage.removeItem('token');
+        window.location.href = '/public/login';
+      }
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config; // muy importante retornar config
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
