7ade463b905a6a797d9e4d4e22a7e0b7caf8c451
Jhoestar <202202883@est.umss.edu>
2025-11-08 20:57:51 -0400

    feature(backend): crear nueva tabla fases

---
+++ backend/database/migrations/2025_10_31_173922_create_competition_phases_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('competition_phases', function (Blueprint $table) {
+            $table->id();
+            $table->string('phase', 50)->unique();
+            $table->text('description')->nullable();
+            $table->boolean('active')->default(false);
+            $table->timestamp('started_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('competition_phases');
+    }
+};
