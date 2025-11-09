11c187f6c8a2eaae603cd896ed53a1272fdc90cd
Jhoestar <202202883@est.umss.edu>
2025-11-08 20:54:22 -0400

    refactor(backend): agregar usuario al autenticarse

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -51,7 +51,8 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => config('jwt.ttl') * 60
+            'expires_in' => config('jwt.ttl') * 60,
+            'user' => Auth::user(),
         ]);
     }
 }
