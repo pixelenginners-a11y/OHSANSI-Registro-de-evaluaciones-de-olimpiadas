7f627d22808f7a012c6ea5d69ab7d375d0728417
Jhoestar <202202883@est.umss.edu>
2025-11-08 21:02:36 -0400

    feature(backend): agregar modelo de competicion

--- backend/app/Models/Listing.php
+++ backend/app/Models/Listing.php
@@ -21,7 +21,7 @@ class Listing extends Model
         'visibility',
     ];
 
-    public function listItems()
+    public function items()
     {
         return $this->hasMany(ListItem::class);
     }
