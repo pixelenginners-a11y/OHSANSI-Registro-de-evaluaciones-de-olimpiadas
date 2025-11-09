28334098659ddb48b9609e6f13d6e640b2fcb1c1
Jhoestar <202202883@est.umss.edu>
2025-11-08 21:12:58 -0400

    feature(backend): agregar los controladores para grupos

---
+++ backend/app/Http/Controllers/GroupMemberController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\GroupMember;
+use Illuminate\Http\Request;
+
+class GroupMemberController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(GroupMember $groupMember)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, GroupMember $groupMember)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(GroupMember $groupMember)
+    {
+        //
+    }
+}
