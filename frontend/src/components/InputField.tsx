5f54209307c36f21ea72ba6c651e2efba55c3a4a
Jhoestar <202202883@est.umss.edu>
2025-12-01 09:56:31 -0400

    fix(frontend): arreglar importaciones y uso de variables

--- frontend/src/components/InputField.tsx
+++ frontend/src/components/InputField.tsx
@@ -1,32 +1,47 @@
-interface InputFieldProps{
+import React from "react";
+
+interface InputFieldProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
-  name: string;
-  type?: React.HTMLInputTypeAttribute;
   error?: string;
-  value?: any;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  required?: boolean;
-  disabled?: boolean;
+  onlyNumbers?: boolean;
 }
 
-export const InputField = ({ label, name, type, error, value, onChange, required, disabled }: InputFieldProps) => {
-    return (
-      <div className="flex flex-col gap-1 mb-3">
-        <label htmlFor={name} className="text-sm font-medium text-gray-700">
-          {label}
-        </label>
-        <input
-          id={name}
-          name={name}
-          type={type}
-          value={value}
-          onChange={onChange}
-          required={required}
-          disabled={disabled}
-          className={`border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-primary-dark ${error ? "border-red-500" : "border-gray-300"}`}
-        />
-        {error && <span className="text-sm text-red-500">{error}</span>}
-      </div>
-    );
-}
+export const InputField = ({
+  label,
+  error,
+  onlyNumbers = false,
+  onChange,
+  ...rest
+}: InputFieldProps) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (onlyNumbers) {
+      const value = e.target.value;
+      if (/^\d*$/.test(value)) {
+        onChange?.(e);
+      }
+    } else {
+      onChange?.(e);
+    }
+  };
 
+  return (
+    <div className="flex flex-col gap-1 mb-3">
+      <label
+        htmlFor={rest.name}
+        className="text-sm font-medium text-gray-700"
+      >
+        {label}
+      </label>
+
+      <input
+        {...rest}
+        onChange={handleChange}
+        className={`border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-primary-dark ${error ? "border-red-500" : "border-gray-300"
+          }`}
+      />
+
+      {error && <span className="text-sm text-red-500">{error}</span>}
+    </div>
+  );
+};
