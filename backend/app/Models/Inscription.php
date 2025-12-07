3e0297d9a8f7f29ce83d7d68eedcc2284d7f4f9d
Jhoestar <202202883@est.umss.edu>
2025-12-07 10:48:01 -0400

    refactor(backend): agregar tipos para inscribed en models

--- backend/app/Models/Inscription.php
+++ backend/app/Models/Inscription.php
@@ -16,9 +16,14 @@ class Inscription extends Model
     ];
 
     const STATUS_INSCRIBED = 'inscribed';
+    const STATUS_CLASSIFIED = 'classified';
     const STATUS_NOT_CLASSIFIED = 'not_classified';
     const STATUS_DISQUALIFIED = 'disqualified';
     const STATUS_FINALIST = 'finalist';
+    const STATUS_GOLD_MEDALIST = 'gold_medalist';
+    const STATUS_SILVER_MEDALIST = 'silver_medalist';
+    const STATUS_BRONZE_MEDALIST = 'bronze_medalist';
+    const STATUS_HONORABLE_MENTION = 'honorable_mention';
 
     public function olympian()
     {
